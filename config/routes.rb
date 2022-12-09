d9addaf61d19fee4336ba348f39c3f67c28b3a8a
nxoo <nxoo.dev@gmail.com>
2022-12-09 23:19:08 +0300

    users routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   post "/signup", to: "sessions#signup"
   resources :questions, only: [:create, :index, :show, :update, :destroy]
   resources :choices, only: [:create, :index, :show, :update, :destroy]
+  resources :users, only: [:index, :show]
 
-  # resources :todos
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
