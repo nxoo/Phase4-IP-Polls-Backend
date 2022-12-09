37811263e97f4bdf6e667f54c443d2b410733c39
nxoo <nxoo.dev@gmail.com>
2022-12-09 13:30:26 +0300

    login controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,4 +15,19 @@ class SessionsController < ApplicationController
     end
   end
 
+  def login
+    user = User.find_by(email: params[:email])
+
+    # you can use bcrypt to password authentication
+    if user && user.password == params[:password]
+
+      # we encrypt user info using the pre-define methods in application controller
+      token = encode_user_data({ user_data: user.id })
+
+      # return to user
+      render json: { token: token, user: user }
+    else
+      render json: { message: "invalid credentials" }
+    end
+  end
 end
