edbdc522a722a17f84fffd692dbdbdab70a27b48
nxoo <nxoo.dev@gmail.com>
2022-12-09 22:58:53 +0300

    show, index for users & show user questions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  def index
+    user = User.all
+    render json: user, include: :questions
+  end
+
+  def show
+    user = User.find(params[:id])
+    render json: user, include: :questions
+  end
+
+  private
+  def render_not_found_response
+    render json: { error: "Error" }, status: :not_found
+  end
 end
