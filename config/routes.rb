869ef86997e8ec8ae53b02af41b4ee3d89c97510
nxoo <nxoo.dev@gmail.com>
2022-12-09 17:02:42 +0300

    added routes for question & choices

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   post "/login", to: "sessions#login"
   post "/signup", to: "sessions#signup"
+  resources :questions, only: [:create, :index, :show, :update, :destroy]
+  resources :choices, only: [:create, :index, :show, :update, :destroy]
 
   # resources :todos
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
